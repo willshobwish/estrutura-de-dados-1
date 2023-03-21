6c355349f96cffa6635d3b5763fb8f2d5cafadce
Willian Murayama <willianyos13@gmail.com>
2023-03-21 13:43:40 -0300

    Corrected insertion function

--- Listas/ListaEstaticaEncadeada.c
+++ Listas/ListaEstaticaEncadeada.c
@@ -6,8 +6,8 @@ struct registro {
     int proximo;
 };
 struct lista {
-    // Info representa a quantidade de elementos ja na lista
-    int info;
+    // tamanho representa a quantidade de elementos ja na lista
+    int tamanho;
     int disponivel;
     int primeiro;
     struct registro A[MAX];
@@ -17,7 +17,7 @@ void InicializarLista(struct lista *lista) {
     int i = 0;
     lista->disponivel = 0;
     lista->primeiro = -1;
-    lista->info = 0;
+    lista->tamanho = 0;
     for (i = 0; i < MAX - 1; i++) {
         lista->A[i].proximo = i + 1;
     }
@@ -25,7 +25,7 @@ void InicializarLista(struct lista *lista) {
 }
 
 void InsereElemento(struct lista *lista, int elemento) {
-    if (lista->info < MAX) {
+    if (lista->tamanho < MAX) {
         if (lista->primeiro = -1) {
             lista->primeiro = lista->disponivel;
             lista->disponivel = lista->disponivel + 1;
@@ -34,10 +34,38 @@ void InsereElemento(struct lista *lista, int elemento) {
         lista->disponivel++;
         lista->A[lista->disponivel].elemento = elemento;
         lista->A[lista->disponivel].proximo = -1;
-        lista->info++;
+        lista->tamanho++;
     } else {
         printf("Lista cheia\n");
     }
+}
+
+void InsereElementoCorreto(struct lista *lista, int elemento) {
+    lista->A[lista->disponivel].elemento = elemento;
+    int auxiliar = lista->A[lista->disponivel].proximo;
+    lista->A[lista->disponivel].proximo = -1;
+    lista->tamanho= lista->tamanho +1;
+    // Se prim != -1 entao
+    if (lista->primeiro = -1) {
+        int i, anterior = -1;
+        i = lista->primeiro;
+        while (i != -1 && lista->A[i].elemento < elemento) {
+            anterior = i;
+            i = lista->A[i].proximo;
+        }
+        if (anterior != -1) {
+            lista->A[lista->disponivel].proximo = lista->primeiro;
+            lista->primeiro = lista->disponivel;
+        } else {
+            if (i != -1) {
+                lista->A[lista->disponivel].proximo = lista->A[anterior].proximo;
+            }
+            lista->A[anterior].proximo = lista->disponivel;
+        }
+    } else {
+        lista->primeiro = lista->disponivel;
+    }
+    lista->disponivel = auxiliar;
 }
 
 void PrintLista(struct lista *lista) {
@@ -54,14 +82,12 @@ void PrintLista(struct lista *lista) {
 int main(void) {
     struct lista listaTeste;
     InicializarLista(&listaTeste);
-    for (int i = 0; i <= MAX; i++) {
-        InsereElemento(&listaTeste, i + 10);
+    for(int i=0;i<MAX;i++){
+        InsereElementoCorreto(&listaTeste,i+10);
     }
-    printf("Primeiro elemento: %d\nDisponivel: %d",listaTeste.primeiro,listaTeste.disponivel);
-    // for (int i = 0; i < MAX; i++) {
-    //     printf("\nElemento %d\nProximo %d\n", listaTeste.A[i].elemento, listaTeste.A[i].proximo);
-    // }
+    printf("Primeiro elemento: %d\nDisponivel: %d", listaTeste.primeiro, listaTeste.disponivel);
+    for (int i = 0; i < MAX; i++) {
+        printf("\nElemento %d\nProximo %d\n", listaTeste.A[i].elemento, listaTeste.A[i].proximo);
+    }
     PrintLista(&listaTeste);
-
-    
 }
