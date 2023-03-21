67dfc4f3f3bdd479bf64325a4559d36a73cb1974
Willian Murayama <willianyos13@gmail.com>
2023-03-21 19:26:06 -0300

    Update bubble_sort.c

--- Sorting_algorithms/bubble_sort.c
+++ Sorting_algorithms/bubble_sort.c
@@ -11,7 +11,6 @@ void swap(int* xp, int* yp) {
 void bubbleSort(int arr[], int n) {
     int i, j;
     for (i = 0; i < n - 1; i++)
-
         // Last i elements are already in place
         for (j = 0; j < n - i - 1; j++)
             if (arr[j] > arr[j + 1])
