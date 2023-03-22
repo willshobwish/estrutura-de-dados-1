e085f2fe730f50ad664aa4e6cf13754cf9b2e64d
Willian Murayama <willianyos13@gmail.com>
2023-03-22 10:06:30 -0300

    Correction of insertion function

--- Listas/ListaEstaticaEncadeada.c
+++ Listas/ListaEstaticaEncadeada.c
@@ -24,19 +24,20 @@ void InicializarLista(struct lista *lista) {
     lista->A[i].proximo = -1;
 }
 
-void Remove(struct lista *lista,int elemento) {
+void Remove(struct lista *lista, int elemento) {
     // Caso a lista nao seja vazia
     if (lista->primeiro != -1) {
         int auxiliar = lista->primeiro;
         int anterior;
-        while(lista->A[auxiliar].elemento < elemento && lista->A[auxiliar].proximo != -1){
+        while (lista->A[auxiliar].elemento < elemento && lista->A[auxiliar].proximo != -1) {
             anterior = auxiliar;
             auxiliar = lista->A[auxiliar].proximo;
-        }if(lista->A[auxiliar].elemento == elemento){
+        }
+        if (lista->A[auxiliar].elemento == elemento) {
             lista->primeiro = auxiliar;
 
-        }else{
-            lista->A[anterior].proximo=lista->A[auxiliar].proximo;
+        } else {
+            lista->A[anterior].proximo = lista->A[auxiliar].proximo;
         }
         int disponivel = lista->disponivel;
         lista->disponivel = auxiliar;
@@ -72,40 +73,58 @@ void Insere(struct lista *lista, int elemento) {
             lista->A[anterior].proximo = lista->primeiro;
             lista->A[auxiliar].proximo = i;
         }
-    }else{
+    } else {
         printf("Lista cheia");
     }
 }
 
-void InsereCorreto(struct lista *lista, int elemento){
-    if(lista->disponivel != -1){
-        int auxiliar = lista->disponivel;
-        lista->A[auxiliar].elemento= elemento;
-        lista->disponivel = lista->A[auxiliar].proximo; 
-    }else{
-        printf("lista cheia");
+void InsereCorreto(struct lista *lista, int elemento) {
+    int auxiliar, i, anterior;
+    if (lista->disponivel != -1) {
+        if (lista->tamanho < MAX) {
+            lista->tamanho = lista->tamanho + 1;
+            auxiliar = lista->disponivel;
+            lista->A[auxiliar].elemento = elemento;
+            lista->disponivel = lista->A[auxiliar].proximo;
+            i = lista->primeiro;
+            // Caso seja 
+            if (lista->primeiro != -1) {
+                while (lista->A[i].elemento < elemento) {
+                    anterior = i;
+                    i = lista->A[i].proximo;
+                }
+            }else{
+                lista->primeiro=lista->disponivel;
+            }
+        } else {
+            lista->disponivel = -1;
+        }
+
+    } else {
+        printf("Lista cheia\n");
     }
 }
 
 void Impressao(struct lista *lista) {
-    int index = lista->primeiro;
+    int auxiliar = lista->primeiro;
     if (lista->primeiro == -1) {
         printf("Nao ha elementos na lista");
     } else {
-        while (lista->A[index].proximo != -1) {
-            printf("Elemento: %d\n", lista->A[index].elemento);
-            index = lista->A[index].proximo;
+        while (lista->A[auxiliar].proximo != -1) {
+            printf("Elemento: %d\n", lista->A[auxiliar].elemento);
+            auxiliar = lista->A[auxiliar].proximo;
         }
     }
 }
 int main(void) {
     struct lista listaTeste;
     InicializarLista(&listaTeste);
-    for (int i = 0; i < MAX; i++) {
+    for (int i = 0; i < MAX - 1; i++) {
         InsereCorreto(&listaTeste, i + 10);
     }
     printf("Primeiro elemento: %d\nDisponivel: %d", listaTeste.primeiro, listaTeste.disponivel);
     for (int i = 0; i < MAX; i++) {
         printf("\nElemento %d\nProximo %d\n", listaTeste.A[i].elemento, listaTeste.A[i].proximo);
     }
+    Impressao(&listaTeste);
 }
