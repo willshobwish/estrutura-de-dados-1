d0a04bd872d0562f1135d6ea1c5f2a061ab1668c
Willian Murayama <willianyos13@gmail.com>
2023-03-28 10:00:28 -0300

    Update string.c

--- Atividadde01/string.c
+++ Atividadde01/string.c
@@ -10,13 +10,16 @@ struct strdim
 
 struct strdim * inicializa(){
     struct strdim *string = malloc(sizeof(struct strdim));
+    string->anterior=NULL;
+    string->proximo = malloc(sizeof(struct strdim));
     return string;
 }
 
 void insere(struct strdim *string, char elemento){
     struct strdim *anterior, *proximo;
     anterior = string->anterior;
-    proximo = string->proximo;
+    anterior->proximo = string;
+    string->proximo = malloc(sizeof(struct strdim));
     string->c = elemento;
 }
 
@@ -25,7 +28,9 @@ void imprime(struct strdim *string){
 }
 int main(void){
     struct strdim *string=inicializa();
-    printf("Endereco em memoria: %p\n",string);
+    printf("Endereco em memoria: %p\nEndereco para o anterior: %p\nEndereco para o proximo: %p\n",string,string->anterior,string->proximo);
     insere(string,'a');
+    insere(string,'b');
+
     imprime(string);
 }
