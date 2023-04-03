dc7fddd051d5184f85e149c91af44d4c7cdb93ec
Willian Murayama <willianyos13@gmail.com>
2023-04-03 13:17:21 -0300

    Create pilhaMultipla.c

---
+++ Pilha/pilhaMultipla.c
@@ -0,0 +1,21 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 3
+#define MAXPILHA 
+
+typedef struct{
+    int Pilhas[MAX];
+    int base[MAXPILHA];
+    int top[MAXPILHA];
+    int NumeroElementoPilha;
+} PilhaMultipla;
+
+void InicializaPilha(PilhaMultipla *Pilha){
+    int i;
+    Pilha->NumeroElementoPilha = MAX
+}
+
+int main(void){
+
+}
