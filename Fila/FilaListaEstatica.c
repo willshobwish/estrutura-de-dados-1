0082bf8562d1ce71355225cb1b081928444635c3
Willian Murayama <willianyos13@gmail.com>
2023-04-04 15:30:53 -0300

    Create FilaListaEstatica.c

---
+++ Fila/FilaListaEstatica.c
@@ -0,0 +1,67 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 10
+
+typedef struct {
+    int ElementosFila[MAX];
+    int inicio;
+    int fim;
+} Fila;
+
+void inicializacao(Fila *fila) {
+    fila->inicio = 0;
+    fila->fim = 0;
+}
+
+bool vazia(Fila *fila) {
+    if (fila->inicio == fila->fim) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool cheio(Fila *fila) {
+    if (fila->fim == MAX) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+void inserir(Fila *fila, int elemento) {
+    if (cheio(fila)) {
+        printf("Lista cheia\n");
+    } else {
+        fila->ElementosFila[fila->fim] = elemento;
+        fila->fim++;
+    }
+}
+
+void remover(Fila *fila, int *elemento) {
+    if (vazia(fila)) {
+        printf("Lista vazia");
+    } else {
+        *elemento = fila->ElementosFila[fila->inicio];
+        fila->inicio++;
+    }
+}
+
+void obterInicioFila(Fila *fila, int *elemento) {
+    if (vazia(fila)) {
+        printf("Lista vazia");
+    } else {
+        *elemento = fila->ElementosFila[fila->inicio];
+    }
+}
+int main(void) {
+    Fila fila;
+    inicializacao(&fila);
+    inserir(&fila, 10);
+    int elemento;
+    obterInicioFila(&fila, &elemento);
+    printf("Primeiro elemento: %d", elemento);
+    return 0;
+}
