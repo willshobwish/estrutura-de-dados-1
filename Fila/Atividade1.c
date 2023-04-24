d42ce6365ccf9700f57b5c0fec0483a9bdc43055
Willian Murayama <willianyos13@gmail.com>
2023-04-23 22:19:44 -0300

    Feature random number and operation

--- Fila/Atividade1.c
+++ Fila/Atividade1.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <time.h>
 
-struct nodeFila{
+struct nodeFila {
     int elemento;
     struct nodeFila *proximo;
 };
@@ -11,34 +11,40 @@ struct nodeFila{
 typedef struct {
     struct nodeFila *inicio;
     struct nodeFila *fim;
-}Fila;
+} Fila;
 
-void inicializacao(Fila *fila){
+void inicializacao(Fila *fila) {
     fila->inicio = NULL;
     fila->fim = NULL;
 }
 
-bool vazia(Fila *fila){
-    if(fila->inicio == NULL) return true;
-    else return false;
+bool vazia(Fila *fila) {
+    if (fila->inicio == NULL)
+        return true;
+    else
+        return false;
 }
 
-void inserir(Fila *fila, int elemento){
+void inserir(Fila *fila, int elemento) {
     struct nodeFila *newNode = malloc(sizeof(struct nodeFila));
-    if(newNode == NULL) printf("Nao ha memoria\n");
-    else{
+    if (newNode == NULL)
+        printf("Nao ha memoria\n");
+    else {
         newNode->elemento = elemento;
         newNode->proximo = NULL;
-        if(vazia(fila)) fila->inicio = newNode;
-        else fila->fim->proximo = newNode;
-        fila->fim=newNode;
+        if (vazia(fila))
+            fila->inicio = newNode;
+        else
+            fila->fim->proximo = newNode;
+        fila->fim = newNode;
     }
 }
 
-void remover(Fila *fila, int *elemento){
+void remover(Fila *fila, int *elemento) {
     struct nodeFila *auxiliar;
-    if(vazia(fila)) printf("Nao ha elementos para remover na fila\n");
-    else{
+    if (vazia(fila))
+        printf("Nao ha elementos para remover na fila\n");
+    else {
         auxiliar = fila->inicio;
         *elemento = auxiliar->elemento;
         fila->inicio = fila->inicio->proximo;
@@ -46,35 +52,61 @@ void remover(Fila *fila, int *elemento){
     }
 }
 
-void obter_inicio_fila(Fila *fila, int *elemento){
-    if(vazia(fila) )printf("Nao ha elementos na fila\n");
-    else{
+void obter_inicio_fila(Fila *fila, int *elemento) {
+    if (vazia(fila))
+        printf("Nao ha elementos na fila\n");
+    else {
         *elemento = fila->inicio->elemento;
     }
 }
 
-void verifica_chegada(){
-
+void verifica_chegada() {
 }
 
-void delay(int number_of_seconds)
-{
+void delay(int number_of_seconds) {
     // Converting time into milli_seconds
     int milli_seconds = 1000 * number_of_seconds;
- 
+
     // Storing start time
     clock_t start_time = clock();
- 
+
     // looping till required time is not achieved
-    while (clock() < start_time + milli_seconds);
+    while (clock() < start_time + milli_seconds)
+        ;
 }
 
-int main(void){
-    int tempo_de_simulacao = 500, tempo_atual=0, atendimento_caixa[4];
-    while(tempo_atual<=tempo_de_simulacao){
-        printf("Tempo atual: %d\n",tempo_atual);
+int random_number(int intervalo) {
+    return rand() % intervalo;
+}
+
+void operacao(Fila *fila, int numero) {
+    switch (numero) {
+        case 0:
+            printf("Chegada\n");
+            break;
+        case 1:
+            printf("Saque\n");
+            break;
+        case 2:
+            printf("Deposito\n");
+            break;
+        case 3:
+            printf("Transacao\n");
+            break;
+        default:
+            break;
+    }
+}
+
+int main(void) {
+    srand(time(NULL));
+    Fila *fila;
+    int tempo_de_simulacao = 10, tempo_atual = 0, intervalo = 10;
+    while (tempo_atual <= tempo_de_simulacao) {
+        printf("Tempo atual: %d\n", tempo_atual);
         tempo_atual++;
+        printf("%d\n", random_number(intervalo));
+        operacao(fila, random_number(4));
         delay(1);
     }
-
 }
