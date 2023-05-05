aef0aef870827c011a53ed39599c8a1349d8f3ff
Willian Murayama <willianyos13@gmail.com>
2023-05-05 18:54:03 -0300

    Feature print

--- Fila/ListaEncadeadaDinamica.c
+++ Fila/ListaEncadeadaDinamica.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct nodeFila{
+struct nodeFila {
     int elemento;
     struct nodeFila *proximo;
 };
@@ -10,34 +10,40 @@ struct nodeFila{
 typedef struct {
     struct nodeFila *inicio;
     struct nodeFila *fim;
-}Fila;
+} Fila;
 
-void inicializacao(Fila *fila){
+void inicializacao(Fila *fila) {
     fila->inicio = NULL;
     fila->fim = NULL;
 }
 
-bool vazia(Fila *fila){
-    if(fila->inicio == NULL) return true;
-    else return false;
+bool vazia(Fila *fila) {
+    if (fila->inicio == NULL)
+        return true;
+    else
+        return false;
 }
 
-void inserir(Fila *fila, int elemento){
+void inserir(Fila *fila, int elemento) {
     struct nodeFila *newNode = malloc(sizeof(struct nodeFila));
-    if(newNode == NULL) printf("Nao ha memoria\n");
-    else{
+    if (newNode == NULL)
+        printf("Nao ha memoria\n");
+    else {
         newNode->elemento = elemento;
         newNode->proximo = NULL;
-        if(vazia(fila)) fila->inicio = newNode;
-        else fila->fim->proximo = newNode;
-        fila->fim=newNode;
+        if (vazia(fila))
+            fila->inicio = newNode;
+        else
+            fila->fim->proximo = newNode;
+        fila->fim = newNode;
     }
 }
 
-void remover(Fila *fila, int *elemento){
+void remover(Fila *fila, int *elemento) {
     struct nodeFila *auxiliar;
-    if(vazia(fila)) printf("Nao ha elementos para remover na fila\n");
-    else{
+    if (vazia(fila))
+        printf("Nao ha elementos para remover na fila\n");
+    else {
         auxiliar = fila->inicio;
         *elemento = auxiliar->elemento;
         fila->inicio = fila->inicio->proximo;
@@ -45,19 +51,32 @@ void remover(Fila *fila, int *elemento){
     }
 }
 
-void obterInicioFila(Fila *fila, int *elemento){
-    if(vazia(fila) )printf("Nao ha elementos na fila\n");
-    else{
+void obterInicioFila(Fila *fila, int *elemento) {
+    if (vazia(fila))
+        printf("Nao ha elementos na fila\n");
+    else {
         *elemento = fila->inicio->elemento;
     }
 }
 
-int main(void){
-Fila fila;
-inicializacao(&fila);
-inserir(&fila,10);
-int elemento;
-remover(&fila, &elemento);
-printf("%d\n",elemento);
-return 0;
+void imprimir(Fila *fila) {
+    struct nodeFila *aux = fila->inicio;
+    printf("Elemento | Atual | Proximo\n");
+    while (aux != NULL) {
+        printf("%.3d | %x -> %x\n", aux->elemento, aux, aux->proximo);
+        aux = aux->proximo;
+    }
+}
+
+int main(void) {
+    Fila fila;
+    inicializacao(&fila);
+    for (int i = 0; i < 20; i++) {
+        inserir(&fila, 10 * i + 5);
+    }
+    imprimir(&fila);
+    // int elemento;
+    // remover(&fila, &elemento);
+    // printf("%d\n",elemento);
+    return 0;
 }
