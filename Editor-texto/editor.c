17aa39a6bb8256e7d02179669930399cd075272b
Willian Murayama <willianyos13@gmail.com>
2023-05-23 15:59:24 -0300

    Correção do código

--- Editor-texto/editor.c
+++ Editor-texto/editor.c
@@ -1,3 +1,4 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -9,15 +10,60 @@ struct strdim {
 
 typedef struct strdim strdim;
 
+void inicializa(strdim *string) {
+    string->elemento = NULL;
+    string->proximo = NULL;
+    string->anterior = NULL;
+}
+
 void insere(strdim *string, char elemento) {
-    strdim *inicio = string,*auxiliar=malloc(sizeof(strdim));
-    while (inicio != NULL) {
-        inicio = inicio->proximo;
+    strdim *auxiliar = string, *novo = malloc(sizeof(strdim));
+    novo->proximo = NULL;
+    novo->elemento = elemento;
+    while (auxiliar->proximo != NULL) {
+        auxiliar = auxiliar->proximo;
     }
-    auxiliar->elemento=elemento;
-    auxiliar->anterior=inicio;
-    inicio->proximo=auxiliar;
+    novo->anterior = auxiliar;
+    auxiliar->proximo = novo;
+}
+
+void insere_palavra(strdim *string, char palavra[]) {
+    int i = 0;
+    while (palavra[i] != '\0') {
+        insere(string, palavra[i]);
+        i++;
+    }
+}
+
+void imprime(strdim *string) {
+    strdim *auxiliar = string;
+    while (auxiliar != NULL) {
+        printf("%c", auxiliar->elemento);
+        auxiliar = auxiliar->proximo;
+    }
+    printf("\n");
+}
+
+bool string_igual(strdim *string1, strdim *string2) {
+    bool resultado = false;
+    strdim *aux1 = string1, *aux2 = string2;
+    while (aux1 != NULL) {
+        if (aux1->elemento == aux2->elemento) {
+            resultado = true;
+        } else {
+            resultado = false;
+            break;
+        }
+        aux1 = aux1->proximo;
+        aux2 = aux2->proximo;
+    }
+    return resultado;
 }
 
 int main(void) {
+    strdim string;
+    inicializa(&string);
+    insere_palavra(&string, "abelha");
+    imprime(&string);
+    return 0;
 }
