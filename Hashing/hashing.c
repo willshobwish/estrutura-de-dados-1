09439ce1f4e0d7001ecb417c98c9ba738eb19b72
Willian Murayama <willianyos13@gmail.com>
2023-05-23 12:30:19 -0300

    Update hashing.c

--- Hashing/hashing.c
+++ Hashing/hashing.c
@@ -15,23 +15,43 @@ void inicializa_tabela_hash(aluno *tabela) {
         tabela[i].matricula = -1;
     }
 }
-int hash(aluno elemento) {
-    return (elemento.matricula % 101) % MAX;
+int hash(int matricula) {
+    return (matricula% 101) % MAX;
 }
 
 void inserir(aluno *tabela, aluno elemento) {
-    int chave = hash(elemento);
+    int chave = hash(elemento.matricula);
     tabela[chave] = elemento;
 };
+
+void remocao(aluno *tabela, int matricula){
+    int chave = hash(matricula);
+    if(tabela[chave].matricula==matricula){
+        tabela[chave].matricula=-1;
+        strcpy(tabela[chave].email,"");
+        strcpy(tabela[chave].nome,"");
+    }else{
+        printf("Matricula nao existente\n");
+    }
+}
+
+void busca(aluno *tabela, int matricula){
+    int chave=hash(matricula);
+    if(tabela[chave].matricula==matricula){
+        printf("Matricula: %d\nEmail: %s\nNome: %s\n",tabela[chave].matricula,tabela[chave].email,tabela[chave].nome);
+    }else{
+        printf("Matricula nao encontrada\n");
+    }
+}
 
 void imprimir(aluno *tabela) {
     for (int i = 0; i < MAX; i++) {
         if (tabela[i].matricula != -1) {
-            printf("%d %d %s %s\n", i, tabela[i].matricula, tabela[i].email, tabela[i].nome);
+            printf("%d %3d %s %s\n", i, tabela[i].matricula, tabela[i].email, tabela[i].nome);
         } else {
-            printf("%d -- Vazio\n", i);
+            printf("%d --- Vazio\n", i);
         }
-    }
+    }printf("\n");
 }
 
 aluno insere_informacoes(int matricula,char email[],char nome[]){
@@ -44,10 +64,10 @@ aluno insere_informacoes(int matricula,char email[],char nome[]){
 int main(void) {
     aluno tabHash[MAX], a;
     inicializa_tabela_hash(&tabHash);
-    // a.matricula = 230;
-    // strcpy(a.email, "teste@teste");
-    // strcpy(a.nome, "teste");
-    
     inserir(&tabHash, insere_informacoes(230,"email@teste.com","nome teste"));
+    inserir(&tabHash, insere_informacoes(23,"email@teste2.com","nome teste 2"));
     imprimir(&tabHash);
+    remocao(&tabHash,230);
+    imprimir(&tabHash);
+    busca(&tabHash,23);
 }
