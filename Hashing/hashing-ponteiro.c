76c83f9bf0094d4928051dbcaf5d59f3e19143b3
Willian Murayama <willianyos13@gmail.com>
2023-05-23 12:30:15 -0300

    Create hashing-ponteiro.c

---
+++ Hashing/hashing-ponteiro.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 10
+
+struct aluno {
+    int matricula;
+    char nome[80];
+    char email[80];
+};
+typedef struct aluno aluno;
+
+int hash(int matricula) {
+    return (matricula % 101) % MAX;
+}
+
+void inicializar(aluno *tabela[]) {
+    for (int i = 0; i < MAX; i++) {
+        tabela[i] = NULL;
+    }
+}
+
+void inserir(aluno *tabela[], int matricula, char email[], char nome[]) {
+    int chave = hash(matricula);
+    aluno *auxiliar;
+    auxiliar = malloc(sizeof(aluno));
+    auxiliar->matricula = matricula;
+    strcpy(auxiliar->email, email);
+    strcpy(auxiliar->nome, nome);
+    tabela[chave] = auxiliar;
+}
+void remocao(aluno *tabela[], int matricula){
+    int chave = hash(matricula);
+    aluno *auxiliar = tabela[chave];
+    if(auxiliar->matricula==matricula){
+        free(auxiliar);
+        tabela[chave]=NULL;
+    }else{
+        printf("Matricula nao existente\n");
+    }
+}
+
+void busca(aluno *aluno[], int matricula){
+    
+}
+
+void imprimir(aluno *tabela[]) {
+    for (int i = 0; i < MAX; i++) {
+        if (tabela[i] != NULL) {
+            printf("%d %d %s %s\n", i, tabela[i]->matricula, tabela[i]->email, tabela[i]->nome);
+        } else {
+            printf("%d --- Vazio\n", i);
+        }
+    }printf("\n");
+}
+int main(void) {
+    aluno tabela[MAX];
+    inicializar(&tabela);
+    inserir(&tabela,10,"teste@","teste");
+    imprimir(&tabela);
+    remocao(&tabela,10);
+    imprimir(&tabela);
+    return 0;
+}
