d38fd03f5c72268d6f7c07f381190e1c4d0640eb
Willian Murayama <willianyos13@gmail.com>
2023-05-30 16:59:02 -0300

    Implementação de conversão de string para char

--- Editor-de-texto-java/src/main/java/controller/Controller.java
+++ Editor-de-texto-java/src/main/java/controller/Controller.java
@@ -10,4 +10,31 @@ package controller;
  */
 public class Controller {
 
+    private static final Controller controller = new Controller();
+
+    private Controller() {
+    }
+
+    /**
+     * Padrão singleton
+     *
+     * @return
+     */
+    public static Controller getInstance() {
+        return controller;
+    }
+
+    public void converterString(String entrada, boolean espaco, boolean quebraDeLinha) {
+        char[] caracteres = entrada.toCharArray();
+//        System.out.println("");
+        for (char e : caracteres) {
+            System.out.print(e);
+        }
+        if (espaco) {
+            System.out.print(" ");
+        }
+        if (quebraDeLinha) {
+            System.out.println("");
+        }
+    }
 }
