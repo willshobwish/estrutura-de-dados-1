9e451d98f20146202c89c38fb011451c88e7f9be
Willian Murayama <willianyos13@gmail.com>
2023-06-13 13:54:11 -0300

    Implementação de alocação dinamica

--- Arvores/binaria.c
+++ Arvores/binaria.c
@@ -1,36 +1,69 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct no{
+struct no {
     int elemento;
     struct no *direita;
     struct no *esquerda;
 };
 
-
 // typedef struct no* arvore;
 typedef struct no no;
 
-void percorre(no *raiz){
-    if(raiz==NULL){
+no *criano() {
+    no *newno = (no *)malloc(sizeof(no));
+    newno->esquerda = NULL;
+    newno->direita = NULL;
+    return newno;
+}
+
+void preordem(no *raiz) {
+    if (raiz == NULL) {
         return 0;
-        }
-    if(raiz!=NULL){
-        printf("%d\n",raiz->elemento);
-        percorre(raiz->esquerda);
-        percorre(raiz->direita);
+    }
+    if (raiz != NULL) {
+        printf("%d\n", raiz->elemento);
+        preordem(raiz->esquerda);
+        preordem(raiz->direita);
     }
 }
 
-int main(void){
+void emordem(no *raiz) {
+    if (raiz == NULL) {
+        return 0;
+    }
+    if (raiz != NULL) {
+        emordem(raiz->esquerda);
+        printf("%d\n", raiz->elemento);
+
+        emordem(raiz->direita);
+    }
+}
+
+void posordem(no *raiz) {
+    if (raiz == NULL) {
+        return 0;
+    }
+    if (raiz != NULL) {
+        posordem(raiz->esquerda);
+        posordem(raiz->direita);
+        printf("%d\n", raiz->elemento);
+    }
+}
+int main(void) {
     no *arvore;
-    arvore = malloc(sizeof(no));
+    arvore = criano();
     arvore->elemento = 1;
-    arvore->direita = malloc(sizeof(no));
-    arvore->esquerda = malloc(sizeof(no));
-    arvore->direita->elemento=2;
-    arvore->esquerda->elemento=3;
+    arvore->direita = criano();
+    arvore->esquerda = criano();
+    arvore->esquerda->elemento = 2;
+    arvore->direita->elemento = 3;
     // printf("%d",arvore->elemento);
-    percorre(arvore);
+    printf("Pre ordem\n");
+    preordem(arvore);
+    printf("Em ordem\n");
+    emordem(arvore);
+    printf("Pos ordem\n");
+    posordem(arvore);
     return 0;
 }
